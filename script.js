590438e0f7e09c63a77ac0565c8dcd0a2449393f
Abby Benjamin <ablizben@gmail.com>
2021-05-17 11:52:45 -0400

    changing name and fixing nav bar on scroll

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
-'use strict';
-
-$(document).ready(function(){       
-    var scroll_start = 0;
-    var startchange = $('#startchange');
-    var offset = startchange.offset();
-    $(document).scroll(function() { 
-       scroll_start = $(this).scrollTop();
-       if(scroll_start > offset.top) {
-           $('.navigation').css('background-color', '#b94335');
-        } else {
-           $('.navigation').css('background-color', 'transparent');
-        }
-    });
+$(function () {
+   $(document).scroll(function () {
+     var $nav = $(".navigation");
+     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+   });
  });
